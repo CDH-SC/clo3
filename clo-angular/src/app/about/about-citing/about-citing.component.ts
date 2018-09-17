828cf13633d7fdf9212d34b5c678ad71c0d01768
Jerrod Mathis <jerrodmathis95@gmail.com>
2018-09-17 13:27:10 -0400

    All about pages finished

--- clo-angular/src/app/about/about-citing/about-citing.component.ts
+++ clo-angular/src/app/about/about-citing/about-citing.component.ts
@@ -10,6 +10,27 @@ export class AboutCitingComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const windowHeight = window.innerHeight;
+    const headerHeight = document.getElementById('header').clientHeight;
+    const divHeight = document.getElementById('content').clientHeight;
+    const offset = 200;
+    const pageHeight = headerHeight + divHeight + offset;
+
+    // Initial Footer Placement
+    if (windowHeight > pageHeight) {
+      document.getElementById('footer').style.position = 'absolute';
+    } else {
+      document.getElementById('footer').style.position = 'relative';
+    }
+
+    // Adjusts footer position based on window resize
+    window.onresize = function () {
+      if (this.window.innerHeight > (pageHeight)) {
+        this.document.getElementById('footer').style.position = 'absolute';
+      } else {
+        this.document.getElementById('footer').style.position = 'relative';
+      }
+    };
   }
 
 }
