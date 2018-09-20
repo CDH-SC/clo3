7167fe69b7597d82acccc449c2eea0bb771c09ba
Caleb Kitzmann <emperor.pickles@gmail.com>
2018-09-20 16:00:40 -0400

    tweaks to mongo volumes script

--- clo-utils/volume_collections.js
+++ clo-utils/volume_collections.js
@@ -6,19 +6,19 @@ Author: Caleb Kitzmann
 // connect to db
 db = connect("localhost:27017/clo");
 
-var volume_id = 1;
+var volume_id = 2;
 
 db.archive_letter.aggregate([
   { $match: { volume_id: volume_id } },
   { $project: {
-    _id: "$xml_id",
-    docDate: "$docDate",
-    firstpage: "$firstpage",
-    lastpage: "$lastpage",
-    docAuthor: "$docAuthor",
-    sender: "$sender",
-    recipient: "$recipient",
-    textClean: "$textClean",
+      _id: "$xml_id",
+      docDate: "$docDate",
+      firstpage: "$firstpage",
+      lastpage: "$lastpage",
+      docAuthor: "$docAuthor",
+      sender: "$sender",
+      recipient: "$recipient",
+      textClean: "$textClean",
   } },
   { $out: `volume_${volume_id}` }
 ]);
