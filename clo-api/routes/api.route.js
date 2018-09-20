f6c757f51f7a8214e9e1e463417d447ac021b490
Caleb Kitzmann <emperor.pickles@gmail.com>
2018-09-20 15:58:58 -0400

    fixed issues with mongoose model preventing connection to db

--- clo-api/routes/api.route.js
+++ clo-api/routes/api.route.js
@@ -3,10 +3,10 @@ var express = require('express')
 var router = express.Router()
 //require route variables here
 var examples = require('./api/example.route')
-var volumes = require('./api/volume.route')
+var volume = require('./api/volume.route')
 
 //add route modules here
 router.use('/examples', examples);
-router.use('/volumes', volumes);
+router.use('/volume', volume);
 
 module.exports = router;
