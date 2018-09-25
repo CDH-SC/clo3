9f07f08f84ce017f0e5b7242dffa23f6b24ee0e4
Caleb Kitzmann <emperor.pickles@gmail.com>
2018-09-25 14:14:21 -0400

    added regex parsing for the first five sections of each volume, as well as adding the current xml files

--- clo-utils/volume_upload.py
+++ clo-utils/volume_upload.py
@@ -13,7 +13,7 @@ from datetime import datetime # measure the speed of script
 from pprint import pprint # pprint library is used to make the output look pretty
 
 startTime = datetime.now()
-directory = "../xml_archive/"
+directory = "../col_xml_archive/completed/"
 
 # Connect to Mongodb
 client = MongoClient('mongodb://localhost:27017/')
@@ -24,10 +24,15 @@ db = client.clo
 # pages from an xml file #
 # into mongodb.             #
 #############################
-def upload_volume():
-    db.volumes.update_one(
-
-    )
+# def upload_volume():
+#     db.volumes.update_one({
+#     "_id":str(volumeID)
+#     "volume_dates":str(volumeDate)},
+#     {
+#     "$set": {
+#      "letters":lettersArray,
+#      }
+#     })
 
 ###################################
 # iterates through an xml file   #
@@ -39,9 +44,30 @@ def main():
     for filename in os.listdir(directory):
         if filename.endswith(".xml"):
             print filename
+            volumeID = ''.join(re.findall("\d{2}", filename)) # get volume id from filename
             file = open(os.path.join(directory, filename), "r")
             content = file.read()
-            
+            lettersArray = []
+            notesArray = []
+
+            #####
+            # volume-wide sections here
+            ####$
+            # get acknowledgements section
+            acknowledgements = re.findall("<div1 type=\"section\" id=\"ed-%s-acknowledgements\">(.*?)</div1>" % volumeID, content, re.DOTALL)
+            print acknowledgements
+            # get introduction
+            introduction = re.findall("<div1 type=\"section\" id=\"ed-%s-introduction\">(.*?)</div1>" % volumeID, content, re.DOTALL)
+            print introduction
+            # get key-to-references
+            key-to-references = re.findall("<div1 type=\"section\" id=\"ed-%s-key-to-references\">(.*?)</div1>" % volumeID, content, re.DOTALL)
+            # get letters-to-carlyles
+            letters-to-carlyles = re.findall("<div1 type=\"section\" id=\"ed-%s-letters-to-the-carlyles\">(.*?)</div1>" % volumeID, content, re.DOTALL)
+            # get chronology
+            chronology = re.findall("<div1 type=\"section\" id=\"ed-%s-chronology\">(.*?)</div1>" % volumeID, content, re.DOTALL)
+            #####
+            # volume 30 has some additional sections here
+            #####
 
 
 if __name__ == '__main__':
