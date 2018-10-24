6e6b77f4453a2c199d87883805f0b5a24633c5a4
Caleb Kitzmann <emperor.pickles@gmail.com>
2018-10-24 14:55:34 -0400

    inital work on metadata box for image viewer

--- clo-angular/src/app/album-viewer/album-viewer.component.ts
+++ clo-angular/src/app/album-viewer/album-viewer.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Lightbox, LightboxConfig } from 'ngx-lightbox';
+import { Lightbox, LightboxConfig, LightboxEvent, LIGHTBOX_EVENT } from 'ngx-lightbox';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Subscription } from 'rxjs';
 
 import { Album } from '../_shared/models/album';
 import { AlbumService } from '../_shared/_services/album.service';
@@ -8,18 +10,21 @@ import { AlbumService } from '../_shared/_services/album.service';
 @Component({
   selector: 'app-album-viewer',
   templateUrl: './album-viewer.component.html',
-  styleUrls: ['./album-viewer.component.css']
+  styleUrls: ['./album-viewer.component.css'],
 })
 export class AlbumViewerComponent implements OnInit {
 
   album: Album;
   _albums = [];
+  _subscription: Subscription;
 
   constructor(
     private albumService: AlbumService,
     private route: ActivatedRoute,
     private _lightbox: Lightbox,
     private _lightboxConfig: LightboxConfig,
+    private _lightboxEvent: LightboxEvent,
+    private modalService: NgbModal,
   ) { _lightboxConfig.disableScrolling = true; }
 
   ngOnInit() {
@@ -32,17 +37,37 @@ export class AlbumViewerComponent implements OnInit {
 
         for (let i = 1; i <= this.album.images.length; i++) {
           const src = this.album.albumUrl + this.album.images[i].imageUrl;
-          const album = { src: src };
+          const album = { src: src, num: i };
           this._albums.push(album);
         }
       });
   }
 
+// Open/close image Lightbox
   open(index: number): void {
+    this._subscription = this._lightboxEvent.lightboxEvent$.subscribe(event => this._onReceivedEvent(event));
     this._lightbox.open(this._albums, index);
+    this.openModal('num: ' + index);
   }
   close(): void {
     this._lightbox.close();
   }
 
+// Metadata modal
+  openModal(content) {
+    this.modalService.open(content, { windowClass: 'metadataModal', centered: true });
+  }
+
+  // Open metadata when image is viewed
+  _onReceivedEvent(event: any): void {
+    if (event.id === LIGHTBOX_EVENT.CLOSE) {
+      this._subscription.unsubscribe();
+      this.modalService.dismissAll();
+    }
+    if (event.id === LIGHTBOX_EVENT.OPEN) {
+    }
+    if (event.id === LIGHTBOX_EVENT.CHANGE_PAGE) {
+      console.log(event.data);
+    }
+  }
 }
