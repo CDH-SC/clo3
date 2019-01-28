ed01e2e738213fea0aef10684177f5572a023537
Jerrod Mathis <jerrodmathis95@gmail.com>
2019-01-28 14:51:26 -0500

    fixing two boxes issue

--- clo-angular/src/app/volume-content/volume-content.component.ts
+++ clo-angular/src/app/volume-content/volume-content.component.ts
@@ -13,7 +13,6 @@ import { FooterService } from '../_shared/_services/footer.service';
   styleUrls: ['./volume-content.component.css']
 })
 export class VolumeContentComponent implements OnInit {
-
   volume: [Volume];
   volumeId: string;
 
@@ -26,7 +25,7 @@ export class VolumeContentComponent implements OnInit {
   fronticePiece: Object;
   letters: Object[];
 
-  hasManuscript: boolean = false;
+  hasManuscript = false;
   manuscriptUrl: Object[] = [];
 
   constructor(
@@ -42,18 +41,17 @@ export class VolumeContentComponent implements OnInit {
     const id = this.route.snapshot.paramMap.get('id');
     this.volumeId = id;
 
-    this.volumeService.getVolumeById<Volume[]>(id)
-      .subscribe(data => {
-        this.volume = data['data'];
-        this.setKeys();
-        // Setting next and previous volume ids for navigation between volumes
-        this.prevId = this.setVolumeId(this.volumeId, 'prev');
-        this.nextId = this.setVolumeId(this.volumeId, 'next');
-        // Get frontice piece object
-        this.fronticePiece = this.volume['frontice_piece'];
-        // Get letters object
-        this.letters = this.volume['letters'];
-      });
+    this.volumeService.getVolumeById<Volume[]>(id).subscribe(data => {
+      this.volume = data['data'];
+      this.setKeys();
+      // Setting next and previous volume ids for navigation between volumes
+      this.prevId = this.setVolumeId(this.volumeId, 'prev');
+      this.nextId = this.setVolumeId(this.volumeId, 'next');
+      // Get frontice piece object
+      this.fronticePiece = this.volume['frontice_piece'];
+      // Get letters object
+      this.letters = this.volume['letters'];
+    });
   }
 
   setKeys() {
@@ -71,94 +69,94 @@ export class VolumeContentComponent implements OnInit {
             break;
           case 'letters_to_carlyles':
             this.tocKeys.push({
-              'key': k,
-              'title': 'Letters to the Carlyles'
+              key: k,
+              title: 'Letters to the Carlyles'
             });
             break;
           case 'key_to_references':
             this.tocKeys.push({
-              'key': k,
-              'title': 'Key to References'
+              key: k,
+              title: 'Key to References'
             });
             break;
           case 'rival_brothers':
             this.tocKeys.push({
-              'key': k,
-              'title': 'The Rival Brothers: Fragment of a Play by Jane Baillie Welsh'
+              key: k,
+              title: 'The Rival Brothers: Fragment of a Play by Jane Baillie Welsh'
             });
             break;
           case 'biographicalNote':
             this.tocKeys.push({
-              'key': k,
-              'title': 'Biographical Notes'
+              key: k,
+              title: 'Biographical Notes'
             });
             break;
           case 'inMemoriam':
             this.tocKeys.push({
-              'key': k,
-              'title': 'In Memoriam'
+              key: k,
+              title: 'In Memoriam'
             });
             break;
           case 'JWCbyTait':
             this.tocKeys.push({
-              'key': k,
-              'title': 'JWC by Robert Scott Tait'
+              key: k,
+              title: 'JWC by Robert Scott Tait'
             });
             break;
           case 'janeNotebook':
             this.tocKeys.push({
-              'key': k,
-              'title': 'Jane Carlyle Notebook'
+              key: k,
+              title: 'Jane Carlyle Notebook'
             });
             break;
           case 'simpleStory':
             this.tocKeys.push({
-              'key': k,
-              'title': 'Simple Story of My Own First Love'
+              key: k,
+              title: 'Simple Story of My Own First Love'
             });
             break;
           case 'janeJournal':
             this.tocKeys.push({
-              'key': k,
-              'title': 'Jane Welsh Carlyle Journal'
+              key: k,
+              title: 'Jane Welsh Carlyle Journal'
             });
             break;
           case 'geraldineJewsbury':
             this.tocKeys.push({
-              'key': k,
-              'title': 'Geraldine Jewsbury to Froude'
+              key: k,
+              title: 'Geraldine Jewsbury to Froude'
             });
             break;
           case 'ellenTwisleton':
             this.tocKeys.push({
-              'key': k,
-              'title': 'Ellen Twisleton Account of Life at Craigenputtoch'
+              key: k,
+              title: 'Ellen Twisleton Account of Life at Craigenputtoch'
             });
             break;
           case 'auroraComments':
             this.tocKeys.push({
-              'key': k,
-              'title': 'Comments on Aurora Leigh'
+              key: k,
+              title: 'Comments on Aurora Leigh'
             });
             break;
           case 'athanaeumAdvertisements':
             this.tocKeys.push({
-              'key': k,
-              'title': 'Athanaeum Advertisements'
+              key: k,
+              title: 'Athanaeum Advertisements'
             });
             break;
           case 'accounts':
             if (this.volume['accounts'].length > 0) {
               this.tocKeys.push({
-                'key': k,
-                'title': 'Account\'s of JWC\'s Death'
+                key: k,
+                title: 'Account\'s of JWC\'s Death'
               });
             }
             break;
           default:
             this.tocKeys.push({
-              'key': k,
-              'title': k
+              key: k,
+              title: k
             });
             break;
         }
@@ -200,40 +198,41 @@ export class VolumeContentComponent implements OnInit {
   }
 
   goToVolume(volId: string) {
+    this.fronticePiece = null;
+    this.viewContent = null;
     this.volumeId = volId;
     this.tocKeys = [];
-    this.volumeService.getVolumeById<Volume[]>(volId)
-      .subscribe(data => {
-        this.volume = data['data'];
-        this.setKeys();
-        // Setting next and previous volume ids for navigation between volumes
-        this.prevId = this.setVolumeId(this.volumeId, 'prev');
-        this.nextId = this.setVolumeId(this.volumeId, 'next');
-        // Get frontice piece object
-        this.fronticePiece = this.volume['frontice_piece'];
-        // Get letters object
-        this.letters = this.volume['letters'];
-      });
+    this.volumeService.getVolumeById<Volume[]>(volId).subscribe(data => {
+      this.volume = data['data'];
+      this.setKeys();
+      // Setting next and previous volume ids for navigation between volumes
+      this.prevId = this.setVolumeId(this.volumeId, 'prev');
+      this.nextId = this.setVolumeId(this.volumeId, 'next');
+      // Get frontice piece object
+      this.fronticePiece = this.volume['frontice_piece'];
+      // Get letters object
+      this.letters = this.volume['letters'];
+    });
   }
 
   getLetter(xml_id: string) {
     this.fronticePiece = null;
-    this.volumeService.getLetterById(this.volumeId, xml_id)
-      .subscribe(data => {
-        const letter = data['data']['letters'][0];
-        this.viewContent = this.sanitizer.bypassSecurityTrustHtml(letter.docBody);
-        // console.log(letter);
-        // Check if the letter has a manuscript
-        if (letter.hasOwnProperty('manuscript')) {
-          this.hasManuscript = true;
-          for (let i = 0; i < letter.manuscript.length; i++) {
-            this.manuscriptUrl[i] = `assets/manuscripts/${letter.xml_id}/${letter.manuscript[i]}`;
-          }
-          console.log(this.manuscriptUrl);
-        } else {
-          this.hasManuscript = false;
+    this.viewContent = null;
+    this.volumeService.getLetterById(this.volumeId, xml_id).subscribe(data => {
+      const letter = data['data']['letters'][0];
+      this.viewContent = this.sanitizer.bypassSecurityTrustHtml(letter.docBody);
+      // console.log(letter);
+      // Check if the letter has a manuscript
+      if (letter.hasOwnProperty('manuscript')) {
+        this.hasManuscript = true;
+        for (let i = 0; i < letter.manuscript.length; i++) {
+          this.manuscriptUrl[i] = `assets/manuscripts/${letter.xml_id}/${letter.manuscript[i]}`;
         }
-      });
+        console.log(this.manuscriptUrl);
+      } else {
+        this.hasManuscript = false;
+      }
+    });
   }
 
   goToFront() {
