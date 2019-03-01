e60b794fe6f6e8ffb5a80d00c2081789554c7e4b
Jerrod Mathis <jerrodmathis95@gmail.com>
2019-03-01 11:53:06 -0500

    updating image viewer

--- clo-angular/src/app/_shared/models/album.ts
+++ clo-angular/src/app/_shared/models/album.ts
@@ -6,15 +6,26 @@ import { Injectable } from '@angular/core';
   @Injectable()
   export class Album {
     _id: number;
-    thumbnailUrl: string;
-    fullsizeUrl: string;
-    images: [{
-      imageUrl: string;
-      date: string;
-      caption: string;
-      title: string;
-      creator: string;
-    }];
+    imagesFolder: string;
+    images: [
+      {
+        imageUrl: string,
+        metadata: {
+          title: string,
+          description: string,
+          subjects: string[],
+          authors: string[],
+          date: string,
+          genre: string,
+          other_titles: string,
+          notes: string,
+          reproduction_note: string,
+          copyright_information: string,
+          language_note: string,
+          format: string
+        }
+      }
+    ]
   }
 
 export default Album;
