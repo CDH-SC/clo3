58661797bbe59ae5b421d2cb088d4c699a51cf65
Prithvi Tippabhatla <prithvi98@gmail.com>
2019-04-17 13:21:51 -0400

    adding pagination

--- clo-angular/src/app/search-results/search-results.component.ts
+++ clo-angular/src/app/search-results/search-results.component.ts
@@ -1,16 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { SearchResult } from '../_shared/models/searchResult';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { SearchService } from '../_shared/_services/search.service';
+import {NgxPaginationModule} from 'ngx-pagination';
+import {PaginationInstance} from '../../../node_modules/ngx-pagination';
 
 @Component({
   selector: 'app-search-results',
   templateUrl: './search-results.component.html',
-  styleUrls: ['./search-results.component.css']
+  styleUrls: ['./search-results.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SearchResultsComponent implements OnInit {
+
+
+  public config: PaginationInstance = {
+    id: 'custom',
+    itemsPerPage: 10,
+    currentPage: 1
+  };
 
   searchTerm: string;
   // searchResults: SearchResult;
--- clo-api/services/search.services.js
+++ clo-api/services/search.services.js
@@ -33,6 +33,8 @@ exports.searchVolumes = async function(search) {
                 $project: {
                     "_id": 1,
                     "letters.xml_id": 1,
+                    "letters.docAuthor": 1,
+                    "letters.docDate": 1
                 }
             },
             {
