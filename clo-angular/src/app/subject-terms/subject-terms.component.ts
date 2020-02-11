3c48c5460842baa6146c321cae576c557df0c0a8
emperorpickles <emperor.pickles@gmail.com>
2020-02-11 16:08:03 -0500

    created Angular components for handling subject terms

---
+++ clo-angular/src/app/subject-terms/subject-terms.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-subject-terms',
+  templateUrl: './subject-terms.component.html',
+  styleUrls: ['./subject-terms.component.css']
+})
+export class SubjectTermsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
