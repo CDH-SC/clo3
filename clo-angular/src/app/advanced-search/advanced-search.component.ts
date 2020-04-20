483ee47dd78cd875900b22861e3e3f27db298a42
ianm24 <ianmcdowell24@gmail.com>
2020-04-20 11:44:20 -0400

    able to sort advanced-search data

--- clo-angular/src/app/advanced-search/advanced-search.component.ts
+++ clo-angular/src/app/advanced-search/advanced-search.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ElementRef, ViewChild, Inject} from '@angular/core';
 import { Router } from '@angular/router';
-import { DOCUMENT } from '@angular/common'; 
+import { DOCUMENT } from '@angular/common';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-advanced-search',
@@ -35,10 +36,10 @@ export class AdvancedSearchComponent {
     let newQuery = [this.boolOps,this.fields,this.inputs];
     this.queries.push(newQuery);
   }
-  testMethod (event: any) {
+  changeDropDown(event: any) {
     let val = event.srcElement.value;
     let id = event.srcElement.id;
-    console.log(val,id);
+    // console.log(val,id);
     if(id.includes("boolOp")) {
       var inputID = id.replace("boolOp","");
       var inputName = (<HTMLInputElement>document.getElementById(inputID)).name;
@@ -49,5 +50,172 @@ export class AdvancedSearchComponent {
       document.getElementById(inputID).setAttribute("name",inputName.substring(0,3) + event.target.value);
     }
   }
+  // https://www.sitepoint.com/sort-an-array-of-objects-in-javascript/
+  compareValues(key, order = 'asc') {
+    return function innerSort(a, b) {
+      if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+        // property doesn't exist on either object
+        return 0;
+      }
 
+      const varA = (typeof a[key] === 'string')
+        ? a[key].toUpperCase() : a[key];
+      const varB = (typeof b[key] === 'string')
+        ? b[key].toUpperCase() : b[key];
+
+      let comparison = 0;
+      if (varA > varB) {
+        comparison = 1;
+      } else if (varA < varB) {
+        comparison = -1;
+      }
+      return (
+        (order === 'desc') ? (comparison * -1) : comparison
+      );
+    };
+  }
+
+  //checks if a boolean-searchfield combination has been added already
+  checkQuery(resultArray,inputName) {
+    if(resultArray.length < 1) {
+      return [false,0];
+    } else {
+      for(var i = 0; i < resultArray.length; i++) {
+        if(resultArray[i][0].includes(inputName)) {
+          return [true,i];
+        }
+      }
+      return [false,resultArray.length];
+    }
+  }
+
+  startSearch() {
+    var result = [];
+    for(var i = 0; i < this.queryNumber; i++) {
+      var currInputId = "newField".concat((i+1).toString());
+      var currInputName = (<HTMLInputElement>document.getElementById(currInputId)).name;
+      var currInputValue = (<HTMLInputElement>document.getElementById(currInputId)).value;
+      var check = this.checkQuery(result,currInputName);
+      console.log(check);
+      if(check[0]) {
+        result[check[1]][1].push(currInputValue);
+      } else {
+        result.push([currInputName, [currInputValue]]);
+      }
+    }
+    console.log(result); //remove the checks for typeof 
+
+    // var result = Object.keys(req.body).map(function(key) {
+    //   return [key, req.body[key]];
+    // });
+    // var queryString = ""
+    // andArray = [];
+    // orArray = [];
+    // notArray = [];
+    // for(var i = 0; i < result.length; i++) {
+    //   var fieldName = "letters." + result[i][0].substring(3);
+    //   if(result[i][0].includes("AND")) {
+    //     if(typeof(result[i][1]) == typeof("")) {
+    //       match_phrase = {};
+    //       match_phrase[fieldName] = result[i][1];
+    //       andArray.push({
+    //         match_phrase
+    //       });
+    //       queryString += "AND "+ result[i][0].substring(3) + ": " + result[i][1] + "\n";
+    //     } else {
+    //       for(var j = 0; j < result[i][1].length; j++) {
+    //         match_phrase = {};
+    //         match_phrase[fieldName] = result[i][1][j];
+    //         andArray.push({
+    //           match_phrase
+    //         });
+    //         queryString += "AND "+ result[i][0].substring(3) + ": " + result[i][1][j] + "\n";
+    //       }
+    //     }
+    //   } else if(result[i][0].includes("OR")) {
+    //     if(typeof(result[i][1]) == typeof("")) {
+    //       match_phrase = {};
+    //       match_phrase[fieldName] = result[i][1];
+    //       orArray.push({
+    //         match_phrase
+    //       });
+    //       queryString += "OR "+ result[i][0].substring(3) + ": " + result[i][1] + "\n";
+    //     } else {
+    //       for(var j = 0; j < result[i][1].length; j++) {
+    //         match_phrase = {};
+    //         match_phrase[fieldName] = result[i][1][j];
+    //         orArray.push({
+    //           match_phrase
+    //         });
+    //         queryString += "OR "+ result[i][0].substring(3) + ": " + result[i][1][j] + "\n";
+    //       }
+    //     }
+    //   } else if(result[i][0].includes("NOT")) {
+    //     if(typeof(result[i][1]) == typeof("")) {
+    //       match_phrase = {};
+    //       match_phrase[fieldName] = result[i][1];
+    //       notArray.push({
+    //         match_phrase
+    //       });
+    //       queryString += "NOT "+ result[i][0].substring(3) + ": " + result[i][1] + "\n";
+    //     } else {
+    //       for(var j = 0; j < result[i][1].length; j++) {
+    //         match_phrase = {};
+    //         match_phrase[fieldName] = result[i][1][j];
+    //         andArray.push({
+    //           match_phrase
+    //         });
+    //         queryString += "NOT "+ result[i][0].substring(3) + ": " + result[i][1][j] + "\n";
+    //       }
+    //     }
+    //   }
+    // }
+    // queryObject = {
+    //     must: andArray,
+    //     should: orArray,
+    //     must_not: notArray
+    //   };
+    // console.log(queryObject);
+    // console.log(andArray);
+    // console.log(orArray);
+    // console.log(notArray);
+    // client.search({
+    //   size: 45,
+    //   index: 'volumes',
+    //   body: {
+    //     query: {
+    //       nested: {
+    //         path: "letters",
+    //         query: {
+    //           bool: queryObject
+    //         },
+    //         inner_hits : {
+    //           size: 10
+    //         }
+    //       }
+    //     }
+    //   }
+    // },function (error, response,status) {
+    //     if (error){
+    //       console.log("search error: "+error)
+    //     }
+    //     else {
+    //       var searchResults = new Array();
+    //       response.body.hits.hits.forEach(function(hit) {
+    //         hit.inner_hits.letters.hits.hits.forEach(function(inHit){
+    //           console.log(hit._id);
+    //           var result = {
+    //             _id: hit._id,
+    //             source: inHit._source,
+    //             terms: result,
+    //             letter: inHit._source
+    //           }
+    //           searchResults.push(result);
+    //         });
+    //       });
+    //       searchResults.sort(compareValues('score', 'desc'));
+    //       res.render("boolesearch", {searchQuery:queryString, volumes:searchResults});
+    //     }
+    //   });
+  }
 }
--- clo-api/elasticSync.js
+++ clo-api/elasticSync.js
@@ -4,6 +4,8 @@ var mongoose = require("mongoose");
 var mongoosastic=require("mongoosastic");
 var Schema = mongoose.Schema;
 require('dotenv').config();
+const { Client } = require('@elastic/elasticsearch')
+const client = new Client({ node: process.env.ES_HOST })
 
 var Letter = new Schema({
   xml_id: {type:String, es_indexed:true},
@@ -40,19 +42,74 @@ Volume.createMapping(function(err, mapping){
     console.log('error creating mapping (you can safely ignore this)');
     console.log(err);
     throw 'try again';
-  }else{
+  }else{ 
     console.log('mapping created!');
     console.log(mapping);
-    volumeSynchronize();
+    // Converts all XML to string when put into elasticsearch database
+    Volume.find(function(err, allvolumes, count) {
+      console.log(allvolumes.length);
+      var volumes = []
+      for(i = 0; i < allvolumes.length; i++) {
+
+        var volume = {
+          _id: parseInt(allvolumes[i]._id)
+        };
+        console.log(volume["_id"]);
+
+        var letters = [];
+
+        for(j = 0; j < allvolumes[i].letters.length; j++) {
+          letter = {
+            xml_id: allvolumes[i].letters[j].xml_id,
+            docDate: allvolumes[i].letters[j].docDate,
+            firstPage: allvolumes[i].letters[j].firstPage,
+            lastPage: allvolumes[i].letters[j].lastPage,
+            docAuthor: allvolumes[i].letters[j].docAuthor,
+            sender: allvolumes[i].letters[j].sender,
+            recipient: allvolumes[i].letters[j].recipient,
+          };
+
+          if(allvolumes[i].letters[j].sourceNote){
+            var fixedSource = allvolumes[i].letters[j].sourceNote.toString().replace(/<[^>]*>/g, '');
+            letter["sourceNote"] = fixedSource.replace(/\n/g, '');
+          } else {
+            letter["sourceNote"] = "";
+          }
+          bodyString = allvolumes[i].letters[j].docBody.toString().replace(/[0-9]<\/a>/g,'')
+          bodyString = bodyString.replace(/<[^>]*>/g, '');
+          bodyString = bodyString.replace(/\n/g, '');
+          letter["docBody"] = bodyString;
+          
+          var footnotes = [];
+          if(allvolumes[i].letters[j].footnotes) {            
+            for(k = 0; k < allvolumes[i].letters[j].footnotes.length; k++) {
+              footnotes.push(allvolumes[i].letters[j].footnotes[k].toString().replace(/<[^>]*>/g, '').replace(/\n/g, ''));
+            }
+          }
+          letter["footnotes"] = footnotes;
+          letters.push(letter);
+        }
+        volume["letters"] = letters;
+        volumes.push(volume);
+      }
+      volumeSynchronize(volumes);
+    });
   }
 });
 
-function volumeSynchronize () {
+function volumeSynchronize (volumes) {
   var stream = Volume.synchronize({}, {saveOnSynchronize: true})
   var count = 0;
 
   stream.on('data', function(err, doc){
     count++;
+    currVol = getVolume(doc._id,volumes);
+    doc.letters = currVol.letters;
+    doc.save(function(err) {
+      if(err) {
+        console.log("error: ", err);        
+      }
+    });
   });
   stream.on('close', function(){
     console.log('indexed ' + count + ' documents!');
@@ -61,4 +118,12 @@ function volumeSynchronize () {
   stream.on('error', function(err){
     console.log(err);
   });
+}
+
+function getVolume(volID, volumes) {
+  for(var i = 0; i < volumes.length; i++) {
+    if(volumes[i]._id == volID) {
+      return volumes[i];
+    }
+  }
 }
