cc2fa87e8afedfa1d8c0d47d5e4b907d0e39bdf5
kennethj-usc <kennyaj123@gmail.com>
2020-08-12 06:00:59 +0000

    pre-demo commits

---
+++ bin/sync_ES.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+cd ~/clo3 ; source env/bin/activate
+cd clo-api
+
+sudo kill -9 $( ps -aux | grep -e "elastic" | awk '{print $2}' | head -1 )
+rm ./bin/elasticsearch-7.8.0/pid
+
+
+./bin/elasticsearch-7.8.0/bin/elasticsearch -d -p pid 
+
+sleep 20s
+node elasticSync.js
+
+cd ~/clo3
