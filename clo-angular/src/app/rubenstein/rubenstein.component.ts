019ff98531f6202d7d3d722cbaa7119afb240f90
stella.masucci <stella.masucci@gmail.com>
2021-02-22 21:48:04 -0500

    created new rubenstein component, added basic html/css to mimic browse volume look

---
+++ clo-angular/src/app/rubenstein/rubenstein.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rubenstein',
+  templateUrl: './rubenstein.component.html',
+  styleUrls: ['./rubenstein.component.css']
+})
+export class RubensteinComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
