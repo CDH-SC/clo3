650193e1c166817c58a5e615b9bc46be232ad856
m-lambo <mal6@email.sc.edu>
2021-10-21 21:31:03 -0400

    Temporarily putting xml in component's folder for error-checking purposes

--- clo-angular/src/app/index-of-correspondents/index-of-correspondents.component.ts
+++ clo-angular/src/app/index-of-correspondents/index-of-correspondents.component.ts
@@ -1,65 +1,60 @@
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-// import xml2js from 'xml2js';  
-// import { HttpClient, HttpHeaders } from '@angular/common/http'; 
+import xml2js from 'xml2js';  
+import { HttpClient, HttpHeaders } from '@angular/common/http'; 
 
 @Component({
   selector: 'app-index-of-correspondents',
   templateUrl: './index-of-correspondents.component.html',
   styleUrls: ['./index-of-correspondents.component.css']
 })
-export class IndexOfCorrespondentsComponent implements OnInit {
+export class IndexOfCorrespondentsComponent {
 
-  ngOnInit(): void {
+  //ngOnInit(){};
+   // title = 'read-xml-angular8';  
+    public xmlItems: any;  
+    constructor(private _http: HttpClient) { this.loadXML(); }  
+    loadXML() {  
+      this._http.get('./volume-46-correspondent-index.xml',  
+        {  
+          headers: new HttpHeaders()  
+            .set('Content-Type', 'text/xml')  
+            .append('Access-Control-Allow-Methods', 'GET')  
+            .append('Access-Control-Allow-Origin', '*')  
+            .append('Access-Control-Allow-Headers', "Access-Control-Allow-Headers, Access-Control-Allow-Origin, Access-Control-Request-Method"),  
+          responseType: 'text'  
+        })  
+        .subscribe((data) => {  
+          this.parseXML(data)  
+            .then((data) => {  
+              this.xmlItems = data;  
+            });  
+        });  
+    }  
+    parseXML(data) {  
+      return new Promise(resolve => {  
+        var k: string | number,  
+          arr = [],  
+          parser = new xml2js.Parser(  
+            {  
+              trim: true,  
+              explicitArray: true 
+            });  
+        parser.parseString(data, function (err, result) {  
+         var obj = result.Correspondent;
+         for (k in obj.ixe) {  
+            var item = obj.ixe[k]; 
+            arr.push({  
+              entry: item.entry[0]
+            }); 
+          } 
+          resolve(arr);  
+        });  
+      });  
+    }
   }
   /*
-  public xmlItems: any;  
-  constructor(private _http: HttpClient) { this.loadXML(); }  
-  loadXML() {  
-    this._http.get('$CLO_ROOT/clo-xml-archive/volume-46-correspondents-index.xml',  
-      {  
-        headers: new HttpHeaders()  
-          .set('Content-Type', 'text/xml')  
-          .append('Access-Control-Allow-Methods', 'GET')  
-          .append('Access-Control-Allow-Origin', '*')  
-          .append('Access-Control-Allow-Headers', "Access-Control-Allow-Headers, Access-Control-Allow-Origin, Access-Control-Request-Method"),  
-        responseType: 'text'  
-      })  
-      .subscribe((data) => {  
-        this.parseXML(data)  
-          .then((data) => {  
-            this.xmlItems = data;  
-          });  
-      });  
-  }  
-  parseXML(data) {  
-    return new Promise(resolve => {  
-      var k: string | number,  
-        arr = [],  
-        parser = new xml2js.Parser(  
-          {  
-            trim: true,  
-            explicitArray: true  
-          });  
-      parser.parseString(data, function (err, result) {  
-        console.log(result);
-        
-        var obj = result.Entry;  
-        for (k in obj.emp) { 
-          var item = obj.emp[k];  
-          arr.push({  
-            id: item.id[0],  
-            name: item.name[0],  
-            gender: item.gender[0],  
-            mobile: item.mobile[0]  
-          });  
-        }  
-        resolve(arr);   
-      });
-    });  
-  }  
-*/
   generateLetters() {
     var lengthOfAlphabet = 26;
     var letters = new Array<String>(lengthOfAlphabet);
@@ -91,3 +86,4 @@ export class IndexOfCorrespondentsComponent implements OnInit {
 
 //letters = ['A','B','C','D','E','F','G','H','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
+*/
