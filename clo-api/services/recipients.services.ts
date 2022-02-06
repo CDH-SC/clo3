4a84034edb4d5d8e95a33a3ca53191fe6e3d22ec
m-lambo <mal6@email.sc.edu>
2022-02-06 16:30:26 -0500

    Added class that Index needs to call to emit message to adv change

--- clo-api/services/recipients.services.ts
+++ clo-api/services/recipients.services.ts
@@ -3,6 +3,14 @@ import { BehaviorSubject } from 'rxjs';
 
 
 export class RecipientsServices {
-    private publisher = new BehaviorSubject('a recipient');
+    private intermediaryService = new BehaviorSubject('a recipient');
+    currRecipient = this.intermediaryService.asObservable();
+
+    constructor() { }
+    /* Index Component will need to call this method, passing in string of recipient selected, which will get relayed to AdvSearch */
+        addNewRecipient(aRecipient: string) {
+            this.intermediaryService.next(aRecipient);
+        }
+    }
 }
 
